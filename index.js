79fa2b3b0eecdb65d2b26cc2cef59b0472e4e4dd
3NS-NG Home <ngoyal@trynomial.solutions>
2023-09-22 07:57:03 -0400

    autoformat

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-$(function(){
-    let fadeSpeed=500;
+$(function () {
+    let fadeSpeed = 500;
     document.getElementById('mainform').reset();
-    $("#current_reg_y").on("click", function() {
+    $("#current_reg_y").on("click", function () {
         $("#div_absent_app").fadeIn(fadeSpeed);
         $("#div_check_reg").hide();
         $(".noregdiv").hide();
     });
-    $("#current_reg_u").on("click", function() {
+    $("#current_reg_u").on("click", function () {
         $("#div_absent_app").hide();
         $("#div_check_reg").fadeIn(fadeSpeed);
         $(".noregdiv").hide();
     });
-    $("#current_reg_n").on("click", function() {
+    $("#current_reg_n").on("click", function () {
         $("#div_absent_app").hide();
         $("#div_check_reg").hide();
-        $("#div_reg_app").fadeIn(fadeSpeed, function() {
-            $("#div_past_reg").fadeIn(fadeSpeed/2);
+        $("#div_reg_app").fadeIn(fadeSpeed, function () {
+            $("#div_past_reg").fadeIn(fadeSpeed / 2);
         });
     });
-    $("#past_reg_y").on("click", function() {
+    $("#past_reg_y").on("click", function () {
         $("#span_documentation").hide();
         $("#div_mail").fadeIn(fadeSpeed);
     });
-    $("#past_reg_n").on("click", function() {
+    $("#past_reg_n").on("click", function () {
         $("#span_documentation").fadeIn(fadeSpeed);
         $("#div_mail").fadeIn(fadeSpeed);
     });
